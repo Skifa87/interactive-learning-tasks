63a32b6644bf791035d8a5e7cd6f297f6eb0a9fb
Skifa87 <skifa.us@gmail.com>
2021-07-29 18:09:55 +0000

    besit

--- route53/route53.tf
+++ route53/route53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.primary.zone_id
-  name    = "www.example.com"
+resource "aws_route53_record" "blog" {
+  zone_id = Z06598721EUGOSQ26ZBIZ
+  name    = "blog.tatik2021.com"
   type    = "A"
-  ttl     = "300"
+  ttl     = "60"
   records = [127.0.0.1]
